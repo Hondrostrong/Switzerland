01a585be7c577a3676f9f4a4957ab5db84a1c2c5
Hondrostrong <74794353+Hondrostrong@users.noreply.github.com>
2020-11-21 21:50:10 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,220 @@
+var new_lang = {
+
+"lt0": "100% natürlich",
+
+"lt1": "Mehr als 5.000 Stück verkauft",
+
+"lt2": "Mehr als 4.000 Bewertungen",
+
+"lt3": "Rückruf bestellen",
+
+"lt4": "Hondrostrong Creme zur Bekämpfung von Gelenkschmerzen, Arthritis und Arthrose",
+
+"lt5": "Bestellen",
+
+"lt6": "* Anzahl der Verpackungen mit<span style=\"white-space: nowrap;\">Rabatt -<span class=\"pack_count\"></span></span>",
+
+"lt7": "<span class=\"before\"></span>Hergestellt<br>aus einzigartigen<br>Grünschalmuscheln",
+
+"lt8": "<span class=\"before\"></span>Werden<br>nur 1500 Stück<br>jährlich produziert",
+
+"lt9": "<span class=\"before\"></span>Lindert от<br>Gelenkschmerzen und Gelenkschwellungen<br>",
+
+"lt10": "<span class=\"before\"></span>Stellt<br>Gelenke<br>und das Gewebe wieder her",
+
+"lt11": "<span class=\"before\"></span>Hilft bei der<br>Arthritis<br>und der Arthrose",
+
+"lt12": "<span class=\"before\"></span>Effektiv<br>bei der<br>Osteochondrose",
+
+"lt13": "Was sind Grünschalmuscheln?",
+
+"lt14": "<span class=\"iconed gold\">Grünschalmuscheln</span>- sind Muscheln, die an der Küste von Neuseeland leben. Miesmuscheln gehören zum traditionellen Essen  von Maori-Aborigines. Die Vertreter des Maori-Volkes leiden praktisch überhaupt nicht an Erkrankungen der Gelenke oder der Wirbelsäule, selbst im hohen Alter. Studien haben gezeigt, dass dies auf Miesmuscheln zurückzuführen ist, die traditionell reichlich in der Ernährung der Aborigines vorhanden sind. ",
+
+"lt15": "Miesmuscheln enthalten eine große Anzahl von Mucopolysacchariden, die im menschlichen Körper Teil der Interzellularsubstanz des Bindegewebes (einschließlich Knorpel) sind, wodurch sie eine positive Wirkung auf die Gelenke ausüben.  Das Verfahren zur Gewinnung des Extrakts ist ziemlich zeitaufwändig, da während des Herstellungsprozesses keine erhöhten Temperaturen und chemischen Komponenten verwendet werden.",
+
+"lt16": "Bald wurde eine Herstellungstechnologie des Extraktes aus Miesmuscheln entwickelt, die es ermöglicht ihre medizinischen Eigenschaften zu erhalten. Das Verfahren zur Gewinnung des Extrakts ist ziemlich zeitaufwändig, da während des Herstellungsprozesses keine erhöhten Temperaturen und chemischen Komponenten verwendet werden.  ",
+
+"lt17": "Die Formel der Hondrostrong Creme mit stark wirkenden Komponenten:",
+
+"lt18": "<span class=\"iconed n2th gold\">Apitoxin (Bienengift)</span>- Hat eine starke schmerzstillende Wirkung, stellt beschädigtes Knorpelgewebe in den Gelenken wieder her und startet Regenerationsprozesse des Gewebes.",
+
+"lt19": "<span class=\"iconed n3th gold\">Saft der Stechpalmenblätter</span>- Behandelt erfolgreich Arthritis und rheumatische Schmerzen. Hat eine entzündungshemmende Wirkung und eine ausgeprägte antimikrobielle Wirkung der Gattung Streptococcus, die in einigen Fällen Rheuma verursachen.",
+
+"lt20": "Stellt zerstörtes Knorpelgewebe effektiv wieder her",
+
+"lt21": "Beseitigt Schmerzen, Schwellungen und Taubheitsgefühl in den Gelenken",
+
+"lt22": "Verleiht den Gliedmaßen die alte Beweglichkeit wieder zurück",
+
+"lt23": "Stellt zerstörtes Knorpelgewebe effektiv wieder her",
+
+"lt24": "Beseitigt Schmerzen, Schwellungen und Taubheitsgefühl in den Gelenken",
+
+"lt25": "Verleiht den Gliedmaßen die alte Beweglichkeit wieder zurück",
+
+"lt26": "Was macht Hondrostrong besser als andere Mittel zur Behandlung von Gelenken?",
+
+"lt27": "Knorpelerkrankungen, wie die Arthritis, Arthrose, Polyarthritis, Koxarthrose, Osteochondrose sind heutzutage weit unter der Bevölkerung verbreitet. Leider treten sie seit jüngster Zeit immer öfters auch unter jungen Leuten auf.",
+
+"lt28": "In Deutschland ist die Situation katastrophal. Etwa 50% der Bevölkerung leiden unter Gelenkerkrankungen. Das Schlimmste ist aber, dass Menschen in der Behandlung dieser Krankheiten alternative Heilmethoden bevorzugen, was zu späteren Komplikationen führt. Um diese Situation endlich in Griff zu bekommen, wurde dieses effektive Mittel dazu entwickelt, um Erkrankungen des Knorpelgewebes mit Hilfe von „Hondrostrong“ behandeln zu können.",
+
+"lt29": "Zur Einführung dieses Mittels IN ALLE Bevölkerungsgruppen, wird - \"Hondrostrong\" ausschließlich auf der offiziellen Webseite im Internet verkauft. Auf diese Weise lassen sich 70% des Preises an der Fernsehwerbung und  300 - 400% des Apothekenaufschlags einsparen, sodass sich absolut ALLE Bedürftigen “Hondrostrong” leisten können.",
+
+"lt30": "Laut Ergebnissen klinischer Studien wird “Hondrostrong” jegliche Erkrankungen des Knorpelgewebes innerhalb von 1,5-2 Monaten behandeln. Das Mittel erwies sich 2-3 mal besser als absolut ALLE teuren Analoga. ",
+
+"lt31": "Hondrostrong - Creme auf Basis seltener Grünschalmuscheln zur Wiederherstellung von Gelenken",
+
+"lt32": "<span class=\"gold\">Hondrostrong</span>wird nach einem alten Rezept des Maori Volkes Neuseelands hergestellt. Das Rezept wird von Generation zu Generation weitergegeben und bleibt strengs Geheim. Das polynesische Volk übergab das Rezept unserem Unternehmen unter einer Bedingung – es sollten nicht mehr als 1500 Dosen Creme pro Jahr hergestellt werden.",
+
+"lt33": "Es enthält einen wertvollen Extrakt aus Grünschalmuscheln, der mit Apitoxin, Kardamom-Extrakt, Amaranthöl und Saft der Stechpalmenblätter angereichert wird.",
+
+"lt34": "Im Vergleich zu Injektionen beschädigt das Mittel während der Anwendung keine Weichteile und nicht die Struktur der Gelenkkapsel, im Gegenteil, es dringt mit Hilfe der Osmose tief in die Gelenkhöhle ein und stellt die Struktur und Funktion der Gelenkkapsel wieder her. Erreicht wird solch ein Effekt durch die geheime Formel von \"Hondrostrong\".",
+
+"lt35": "Hondrostrong Creme zur Bekämpfung von Gelenkschmerzen, Arthritis und Arthrose",
+
+"lt36": "Bestellen",
+
+"lt37": "* Anzahl der Verpackungen mit einem<span style=\"white-space: nowrap;\">Rabatt -<span class=\"pack_count\"></span></span>",
+
+"lt38": "Die Effektivität der Hondrostrong Creme wurde von Patienten nachgewiesen",
+
+"lt39": "Ich empfehle allen meinen Patienten mit unterschiedlichen Gelenkerkrankungen die Hondrostrong Creme. Wegen der begrenzten Produktion ist die Creme nicht für alle zugänglich. Die Wirkung der Creme wurde lokalisiert und dringt direkt in den Hohlraum der Gelenkkapsel ein und beseitigt die Krankheit.  Die einzigartige Zusammensetzung des Mittels \"Hondrostrong\" regeneriert das beschädigte Knorpelgewebe, lindert Schmerzen, Gelenkentzündungen und stellt eine normale Funktionsfähigkeit wieder her. Eignet sich perfekt für die Behandlung von Osteochondrose, Arthritis, Erkrankungen der Funktion des Stütz- und Bewegungsapparats.   ",
+
+"lt40": "Jürgen Albrecht",
+
+"lt41": "Chirurg der höchsten Kategorie",
+
+"lt42": " Ich empfehle allen meinen Patienten mit unterschiedlichen Gelenkerkrankungen die Hondrostrong Creme. Wegen der begrenzten Produktion ist die Creme nicht für alle zugänglich. Die Wirkung der Creme wurde lokalisiert und dringt direkt in den Hohlraum der Gelenkkapsel ein und beseitigt die Krankheit.  Die einzigartige Zusammensetzung des Mittels \"Hondrostrong\" regeneriert das beschädigte Knorpelgewebe, lindert Schmerzen, Gelenkentzündungen und stellt eine normale Funktionsfähigkeit wieder her. Eignet sich perfekt für die Behandlung von Osteochondrose, Arthritis, Erkrankungen der Funktion des Stütz- und Bewegungsapparats. ",
+
+"lt43": "Jürgen Albrecht",
+
+"lt44": "Chirurg der höchsten Kategorie",
+
+"lt45": "Was haben klinische Studien von Hondrostrong gezeigt?",
+
+"lt46": "Im Herbst 2014 führe man in Deutschland klinische Studien der Hondrostrong Creme durch. Insgesamt nahmen daran mehr als 5000 Menschen, die  an Gelenkerkrankungen leiden, teil. Innerhalb von 40 Tagen wendeten die Probanden 2 mal täglich die Hondrostrong an. Die Ergebnisse des Experiments haben selbst die Ärzte überrascht!  ",
+
+"lt47": "Alle Studienteilnehmer stellten bei der Anwendung der Hondrostrong Creme deutliche Verbesserungen fest. Fast 90% der Menschen haben die Arthritis losgeworden. Kein anderes bisher bekanntes Mittel zur Behandlung von Erkrankungen des Knorpels und des Bindegewebes hat solch eine hohe Effektivität.",
+
+"lt48": "Wer ist an Gelenkerkrankungen gefährdet?",
+
+"lt49": "<span class=\"before\"></span>Unter Gelenkerkrankungen leidende Personen",
+
+"lt50": "<span class=\"before\"></span>Ältere Personen, die aufgrund altersbedingter Veränderungen unter mangelnder Gelenkelastizität leiden",
+
+"lt51": "<span class=\"before\"></span>Personen nach einer Gelenkoperation",
+
+"lt52": "<span class=\"before\"></span>Personen mit Verletzungen der Gelenke und Wirbelsäule",
+
+"lt53": "<span class=\"before\"></span>Personen mit inaktivem Lebensstil",
+
+"lt54": "<span class=\"before\"></span>Personen mit längeren Gelenkbelastungen (auch in Folge des Übergewichts)",
+
+"lt55": "<span class=\"before\"></span>Sportler",
+
+"lt56": "<span class=\"before\"></span>Zur Vorbeugung der Entwicklung von Gelenkerkrankungen bei allen Personen (da sich die Gelenke mit dem Alter verschlechtern)",
+
+"lt57": "Kundenbewertungen über die Creme \"Hondrostrong\"",
+
+"lt58": "Ernst, 45 Jahre",
+
+"lt59": "Habe vor 3 Jahren Knieschmerzen bekommen. Zunächst wurde mir eine Röntgenaufnahme gemacht und gesagt, dass ich unter der Arthritis leide. Ich konnte kaum gehen. Die Ärzte schlugen mir eine OP vor. Ich wollte mich aber nicht operieren lassen und habe angefangen mich mit diesem Problem selbstständig auseinander zu setzen. Gott sei Dank, dass ich nicht auf sie gehört habe. Bin anschließend auf die “Hondrostrong” aufmerksam geworden und diese auf der offiziellen Webseite bestellt. Nach 7 Tagen habe ich sie erhalten und angefangen sie gemäß der Gebrauchsanweisung anzuwenden. Am 11.Tag habe ich mich bereits angefangen für das städtische Marathon vorzubereiten. Mein Knie stört mich nicht mehr!!  ",
+
+"lt60": "Anna, 30 Jahre",
+
+"lt61": "Ich wurde mit der Chondrose und Schmerzen im Schultergelenk (er war angeschwollen) ins Krankenhaus eingewiesen. Dort wurde ich mit Tropfen, Vitaminen, Magneten, Matratzen , Akupunktur und Massage behandelt. Nach einiger Zeit wurde ich aus dem Krankenhaus entlassen, jedoch tat mir mein Arm immer noch weh. Der Neurologe hat mir empfohlen eine neue Entwicklung auszuprobieren. Dabei handelt sich um die Creme “Hondrostrong”, die ich 3 mal täglich verwendet habe. Die Creme hat mir wirklich gut geholfen, kann jetzt ganz in Ruhe arbeiten und tanzen. ",
+
+"lt62": "Anna, 30 Jahre",
+
+"lt63": "Tobias, 62 Jahre",
+
+"lt64": "Freut mich, dass die “Hondrostrong” Creme nicht nur mir geholfen hat! Ich habe mir Sorgen gemacht, dass ich operiert werden muss. Gott sei Dank, dass ich auf einen erfahrenen Arzt gestoßen bin, der mir dieses Medikament verschrieben hat. Nach 3 Behandlungskursen hat sich mein Gelenk erholt, habe Schmerzen noch Schwellungen. Die Creme ist großartig, das steht auf jeden Fall fest.  ",
+
+"lt65": "Gebrauchsanweisung",
+
+"lt66": "Tragen Sie eine kleine Menge Creme auf die Handfläche auf",
+
+"lt67": "Tragen Sie die Creme mit leichten Massagebewegungen auf die Problemstelle auf",
+
+"lt68": "Tragen Sie die Creme 3 mal<span style=\"white-space: nowrap;\">täglich auf</span>",
+
+"lt69": "Tragen Sie eine kleine Menge Creme auf die Handfläche auf",
+
+"lt70": "Tragen Sie die Creme mit leichten Massagebewegungen auf die Problemstelle auf",
+
+"lt71": "Tragen Sie die Creme 3 mal<span style=\"white-space: nowrap;\">täglich auf</span>",
+
+"lt72": "Achtung! Hüten Sie sich vor Fälschungen!",
+
+"lt73": "Grünschalmuscheln sind für ihre heilenden Eigenschaften bekannt. Die Creme Hondrostrong wird nach einem alten und geheimen Rezept des polynesischen Volkes hergestellt. Grünschalmuscheln werden nur an der Küste Neuseelands gesammelt.",
+
+"lt74": "Hüten Sie sich vor Fälschungen! Wenn Sie eine Creme basierend auf Grünschalmuscheln und zu einem günstigen Preis sehen, handelt es sich höchstwahrscheinlich um eine Fälschung. Im besten Fall wird es keine Wirkung geben, im schlimmsten Fall wird sich die Krankheit weiterentwickeln. Die Originalcreme finden Sie auf unserer Webseite. Wir sind offizieller Lieferant und garantieren eine gute Qualität unserer Produkte.",
+
+"lt75": "Lieferung und Zahlung:",
+
+"lt76": "<span class=\"before\"></span>Hinterlassen Sie auf der Webseite eine Anfrage",
+
+"lt77": "<span class=\"before\"></span>Innerhalb von 15 Minuten werden unsere Kundenberater Sie zur Bestätigung Ihrer Bestellung anrufen",
+
+"lt78": "<span class=\"before\"></span>Wir verpacken und versenden Ihre Bestellung",
+
+"lt79": "<span class=\"before\"></span>Nach 7-14 Tagen erhalten Sie Ihre Bestellung",
+
+"lt80": "<span class=\"before\"></span>Zahlung erfolgt bei Erhalt Ihrer Bestellung ",
+
+"lt81": "Fachkräfte unseres Unternehmens",
+
+"lt82": "Fritz Müller",
+
+"lt83": "Qualitätsleiter",
+
+"lt84": "stellt sicher, dass Ihre Bestellung korrekt verpackt ist",
+
+"lt85": "Anna Schulz",
+
+"lt86": "Leiterin des Call-Centers",
+
+"lt87": "verantwortlich für eine rechtzeitige Kommunikation mit unseren Kunden",
+
+"lt88": "Stefan Klinsmann",
+
+"lt89": "Logistikleiter",
+
+"lt90": "verantwortlich für die Lieferung des Pakets an die richtige Adresse und zur richtigen Zeit",
+
+"lt91": "Fritz Müller",
+
+"lt92": "Qualitätsleiter",
+
+"lt93": "stellt sicher, dass Ihre Bestellung korrekt verpackt ist",
+
+"lt94": "Anna Schulz",
+
+"lt95": "Leiterin des Call-Centers",
+
+"lt96": "verantwortlich für eine rechtzeitige Kommunikation mit unseren Kunden",
+
+"lt97": "Stefan Klinsmann ",
+
+"lt98": "Logistikleiter",
+
+"lt99": "verantwortlich für die Lieferung des Pakets an die richtige Adresse und zur richtigen Zeit",
+
+"lt100": "Hondrostrong Creme zur Bekämpfung von Gelenkschmerzen, Arthritis und Arthrose",
+
+"lt101": "Bestellen",
+
+"lt102": "* Anzahl der Verpackungen mit<span style=\"white-space: nowrap;\">Rabatt -<span class=\"pack_count\"></span></span>",
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
